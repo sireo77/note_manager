fea8aad310e1a2430afb551499d9e8979d41b641
kkonev <konstantin.konev@iplc.ru>
2025-01-13 10:18:50 +0500

    10/01

---
+++ greetings.py
@@ -0,0 +1,12 @@
+username = "Unexpected Name"
+title = "Funny title"
+content = "Exciting content"
+status = "New"
+created_date = "01-01-1980"
+issue_date = "01-01-1980"
+print("Имя пользователя:", username)
+print("Заголовок:", title)
+print("Содержимое:", content)
+print("Статус заметки:", status)
+print("Дата создания:", created_date)
+print("Дата выполнения:", issue_date)
