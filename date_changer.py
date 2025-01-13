4c6138047cb8fd41f079e89d3d10dac68e8e9afe
kkonev <konstantin.konev@iplc.ru>
2025-01-13 11:21:10 +0500

    13/01

--- date_changer.py
+++ date_changer.py
@@ -1,4 +1,4 @@
-created_date = "01-01-1980"
-issue_date = "01-01-1980"
+created_date = "01/01/1980"
+issue_date = "01/01/1980"
 print("Сокр. дата создания:",created_date[0:5])
 print("Сокр. дата выполнения:", issue_date[0:5])
