1b14f5a6155a6f56ab10d46b98df750db82cc302
kkonev <konstantin.konev@iplc.ru>
2025-01-14 14:28:55 +0500

    13/01

--- date_changer.py
+++ date_changer.py
@@ -2,7 +2,3 @@ created_date = "01/01/1980"
 issue_date = "01/01/1980"
 print("Сокр. дата создания:",created_date[0:5])
 print("Сокр. дата выполнения:", issue_date[0:5])
-
-a_note = ["", "", "", "", ""]  # Основной список для хранения заметки
-a_note[0] = created_date
-print(a_note[0][0:5])
