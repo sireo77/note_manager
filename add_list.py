e193d7365c10c89fc3092464087377002e3af4c7
kkonev <konstantin.konev@iplc.ru>
2025-01-15 15:44:29 +0500

    15/01

--- add_list.py
+++ add_list.py
@@ -1,6 +1,12 @@
-title_list = []
-title_count = 3
-for i in range(0, title_count):
+title_list = [] # Задание массива (списка)
+title_count = 3 # Задание числа заголовков
+for i in range(0, title_count): # Цикл ввода и помещения в список заголовков
     title = input("Введите заголовок заметки №" + str(i+1)+ ": ")
     title_list.append(title)
-print("Перечень заголовков:", title_list)
+# print("Перечень заголовков:", title_list) # Краткий формат вывода
+
+# Подробный вывод
+print(f"Cписок заголовков заметки: "
+      f"первый - {title_list[0]}, "
+      f"второй - {title_list[1]}, "
+      f"третий - {title_list[2]}.")
