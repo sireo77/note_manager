e16fdb153950224bc6397bbc12bf5f65a7517671
kkonev <konstantin.konev@iplc.ru>
2025-01-20 11:13:11 +0500

    20/01

--- multiple_notes.py
+++ multiple_notes.py
@@ -1,15 +1,63 @@
-# Код по спецификации № 2.4 по Grade 1:
+# Код по спецификации № 2.5 по Grade 1:
 # Префикс "a" обозначает локальную переменную, "t" - временную, промежуточную
 
 # Загрузка нужных модулей для работы
-import ast, datetime
+import ast
+import datetime
+
+
+# Функция оформления строки вывода заголовков
+# По заданию нужно отображать только последний заголовок, поэтому функция не требуется
+# def nice_string_print(t_list):
+#     t_title_list_str = ""  # Пустая строка для создания красивого вывода
+#     t_title_count = len(t_list["6"])  # Получение числа заголовков
+#     # Цикл для заполнения строки заголовков с пояснениями
+#     for i in range(0, t_title_count):
+#         if i == 0:
+#             t_title_list_str = t_title_list_str + "Заголовок №1 - " + t_list["6"][0] + "; "
+#         else:
+#             t_title_list_str = t_title_list_str + "заголовок №" + str(i + 1) + " - " + t_list["6"][i] + "; "
+#     t_title_list_str = t_title_list_str[:-2] + "."  # Меняем точку с запятой на точку в конце предложения
+#     return t_title_list_str
+
+
+# Функция вывода меню
+def show_choose_user_actions(t_menu_list):  #
+    if len(t_menu_list) == 0:
+        print("Список заголовков меню пуст!")
+        exit()
+    print("Введите цифру для выбора варианта работы с заметками:")
+    for i in range(0, len(t_menu_list)):
+        print(f"'{i}' - {t_menu_list[i]};")
+    t_act = str(input("Выберите нужное число: > "))
+    return t_act
+
+
+# Функция форматированного вывода
+def show_note(t_list, t_num_note):
+    # t_title_list_str = nice_string_print(t_list)  # Вызов функции оформления строки вывода заголовков
+    # Подробный вывод
+    print(f"{t_num_note + 1}. Текущий заголовок заметки: {t_list["6"][len(t_list["6"])-1]}")
+    print(f"   Пользователь {t_list["1"]}, написал заметку о {t_list["2"]}, присвоив ей статус - '{t_list["3"]}'.")
+    print(f"   Дата записи - {t_list["4"]}, cрок выполнения - {t_list["5"]}.")
+
+
+# Функция форматированного отображения всех заметок
+def show_all_notes(t_lists):  #
+    print("===== Перечень заметок: =====")
+    for i in range(0, len(t_lists)):
+        show_note(t_lists[i], t_lists.index(t_lists[i]))
+        # return t_act
+
 
 # Функция заполнения списка заголовков
-def TitlesInput(t_list):
+# Идея в том, чтобы хранить историю заголовков и возвращать последний
+def titles_input(t_list):
     t_exit_allow = "1"  # Инициализация маркера выхода из режима ввода заголовков
     # Цикл ввода заголовков
     t_title_count = 0  # Инициализация числа заголовков
     t_title_list = t_list["6"]
+    print("Введите историю заголоовков - от старых к новым:")
     while t_exit_allow == "1":  # Сторожевое условие
         t_title = input("Введите заголовок заметки №" + str(t_title_count + 1) + ": ")
         t_title_list.append(t_title)
@@ -19,25 +67,42 @@ def TitlesInput(t_list):
             t_exit_allow = "1"
             print("Вы ввели некорректное число, поэтому ввод заголовков будет продолжен")
     t_list["6"] = t_title_list  # Внесение списка заголовков в список заметок note
-    t_list["7"] = t_title_count  # Внесение числа заголовков
     return t_list
 
-# Функция оформления строки вывода заголовков
-def NiceSringPrint(t_list):
-    t_title_list_str = ""  # Пустая строка для создания красивого вывода
-    t_title_count = t_list["7"] # Инициализация числа заголовков
-    # Цикл для заполнения строки заголовков с пояснениями
-    for i in range(0, t_title_count):
-        if i == 0:
-            t_title_list_str = t_title_list_str + "Заголовок №1 - " + t_list["6"][0] + "; "
-        else:
-            t_title_list_str = t_title_list_str + "заголовок №" + str(i + 1) + " - " + t_list["6"][i] + "; "
-    t_title_list_str = t_title_list_str[:-2] + "."  # Меняем точку с запятой на точку в конце предложения
-    return t_title_list_str
+
+# Функция ввода новой заметки и добавления в общий список заметок
+def new_note(t_lists, t_list):  # t_lists пока не используется
+    print("===== Функция добавления заметок: =====")
+    t_exit_allow = "1"  # Инициализация маркера выхода
+    while t_exit_allow == "1":  # Сторожевое условие
+        # Ввод данных о заметке
+        t_list_in = t_list
+        t_list_in["1"] = input("Введите имя пользователя: ")
+        t_list_in["2"] = input("Введите саму заметку: ")
+        t_list_in = titles_input(t_list_in)  # Вызов функции заполнения списка заголовков
+        t_list_in["3"] = status_change(t_list_in)  # Вызов функции изменения статуса заметки
+        t_list_in["4"] = input_date(a_note["4"], "создания заметки")
+        t_list_in["5"] = input_date(a_note["5"], "выполнения")
+        t_lists = append_notes(t_lists, t_list_in)
+        show_note(t_list_in, t_lists.index(t_list_in))
+        t_exit_allow = str(input("Ввести ещё заметку (нажмите ""1"") или выйти (любая другая) > "))
+    print("===== Ввод заметок закончен =====")
+    save_notes(t_lists)
+
+
+# Функция удаления заметки в общем списке заметок
+def del_note(t_lists, t_list):  #
+    print("===== Функция удаления заметок по ключам: =====")
+    print("Извините, данная функция пока не доступна")
+    # t_exit_allow = "1"  # Инициализация маркера выхода
+    # while t_exit_allow == "1":  # Сторожевое условие
+    #
+    # print("===== Удаление заметок завершено =====")
+    # save_notes(t_lists)
 
 # Функция изменения статуса заметки
-def StatusChange(t_list):
-    t_exit_allow = False # Инициализация маркера выхода из режима выбора статуса
+def status_change(t_list):
+    t_exit_allow = False  # Инициализация маркера выхода из режима выбора статуса
     while not t_exit_allow:
         print(f"Для уточнения статуса нажмите: 1 - статус '{a_status_list[0]}', "
               f"2 - статус '{a_status_list[1]}', 3 - статус '{a_status_list[2]}'.")
@@ -55,54 +120,29 @@ def StatusChange(t_list):
             print("Вы ввели некорректное значение. Повторите ввод, пожалуйста!")
     return t_list["3"]
 
-# Функция форматированного вывода
-def ShowNote(t_list, t_num_note):
-    t_title_list_str = ""  # Пустая строка для создания красивого вывода
-    t_title_count = len(t_list["6"]) # Получение числа заголовков
-    t_title_list_str = NiceSringPrint(t_list) # Вызов функции оформления строки вывода заголовков
-    # Подробный вывод
-    print(f"Содержимое заметки №{t_num_note + 1}:")
-    print(f"Пользователь {t_list["1"]}, написал заметку о {t_list["2"]}, присвоив ей статус - '{t_list["3"]}'.")
-    print(f"Cписок заголовков заметки: {t_title_list_str}")
-    print(f"Дата записи - {t_list["4"]}, cрок выполнения - {t_list["5"]}.")
-
-# Функция ввода новой заметки и добавления в общий список заметок
-def NewNote(t_lists, t_list): #t_lists пока не используется
-    t_exit_allow = "1"  # Инициализация маркера выхода
-    while t_exit_allow == "1":  # Сторожевое условие
-        # Ввод данных о заметке
-        t_list_in = t_list
-        t_list_in["1"] = input("Введите имя пользователя: ")
-        t_list_in["2"] = input("Введите саму заметку: ")
-        t_list_in = TitlesInput(t_list_in)  # Вызов функции заполнения списка заголовков
-        t_list_in["3"] = StatusChange(t_list_in)  # Вызов функции изменения статуса заметки
-        t_list_in["4"] = InputDate(a_note["4"], "создания заметки")
-        t_list_in["5"] = InputDate(a_note["5"], "выполнения")
-        t_lists = AppendNotes(t_lists, t_list_in)
-        ShowNote(t_list_in, t_lists.index(t_list_in))
-        t_exit_allow = str(input("Ввести ещё заметку (нажмите ""1"") или выйти (любая другая) > "))
-    print("===== Ввод заметок закончен =====")
-    SaveNotes(t_lists)
 
 # Функция для получения текущей даты
-def GetToday():
+def get_today():
     t_date = datetime.datetime.now()
     return t_date
 
+
 # Функция для расчёта разницы между датами
-def QuantDays(t_list):
-    t_day_count = abs((datetime.datetime.strptime(t_list["5"], "%d-%m-%Y") - GetToday()).days)
+def quant_days(t_list):
+    t_day_count = abs((datetime.datetime.strptime(t_list["5"], "%d-%m-%Y") - get_today()).days)
     return t_day_count
 
+
 # Функция для обработки пользовательского ввода дат
-def InputDate(t_date, t_definition):
+def input_date(t_date, t_definition):
     t_exit_allow = False  # Инициализация маркера выхода
     while not t_exit_allow:
-        t_date = input(f'Введите дату {t_definition} в формате (dd-mm-yyyy), используя ""-"" в качестве разделителя: > ')
+        t_date = input(
+            f'Введите дату {t_definition} в формате (dd-mm-yyyy), используя ""-"" в качестве разделителя: > ')
         date_object = datetime.datetime.strptime("01-01-2025", "%d-%m-%Y").date()
         # Проверка того, что ввод был в нужном формате без вылета из программы
         try:
-            t_exit_allow = True   # Разрешаем выход, если нет ошибки
+            t_exit_allow = True  # Разрешаем выход, если нет ошибки
             date_object = datetime.datetime.strptime(t_date, "%d-%m-%Y").date()
         except:
             t_exit_allow = False  # Запрещаем выход из цикла, пока есть ошибка
@@ -111,118 +151,103 @@ def InputDate(t_date, t_definition):
     print(f"Введена дата {succes_date_string}. Она соответствует формату.")
     return succes_date_string
 
+
 # Функция проверки срока выполнения в текущей записи
-def DataCheck(t_list):
-    if datetime.datetime.strptime(t_list["5"], "%d-%m-%Y") < GetToday():  #
-        print(f"Срок истёк. Просрочка составляет дней: {(QuantDays(t_list))}.")
-    elif datetime.datetime.strptime(t_list["5"], "%d-%m-%Y") == GetToday():  #
+def data_check(t_list):
+    if datetime.datetime.strptime(t_list["5"], "%d-%m-%Y") < get_today():  #
+        print(f"Срок истёк. Просрочка составляет дней: {(quant_days(t_list))}.")
+    elif datetime.datetime.strptime(t_list["5"], "%d-%m-%Y") == get_today():  #
         print(f"Сегодня дедлайн! Нужно поторопиться!.")
     else:
-        print(f"Срок ещё не наступил. До дедлайна осталось дней: {(QuantDays(t_list))}.")
-        t_exit_allow = False
+        print(f"Срок ещё не наступил. До дедлайна осталось дней: {(quant_days(t_list))}.")
     return t_list
 
+
 # Функция сохранения переменной notes в промежуточном файле notes.txt
-def SaveNotes(t_lists):
+def save_notes(t_lists):
     with open('notes.txt', 'w+') as f:
         f.write(str(t_lists))
 
+
 # Функция выгрузки списка из промежуточного файла notes.txt
-def LoadNotes():
+def load_notes():
     with open('notes.txt', 'r+') as f:
         t_str = f.read()
-    t_lists = ast.literal_eval(t_str) # преобразуем строку в список
+    t_lists = ast.literal_eval(t_str)  # преобразуем строку в список
     return t_lists
 
+
 # Функция добавления заметки note в список заметок notes
-def AppendNotes(t_lists, t_list): # Первый аргумент общий список notes, второй - note
-    t_lists = LoadNotes()
+def append_notes(t_lists, t_list):  # Первый аргумент общий список notes, второй - note
+    t_lists = load_notes()
     t_lists.append(t_list)
     return t_lists
 
+
 # Функция проверка наличия заметок note в списке заметок notes
-def CheckNotesInList(): #
+def check_notes_in_list():  #
     t_list_not_empty = False
-    t_lists = LoadNotes()
+    t_lists = load_notes()
     if len(t_lists) > 0:
         t_list_not_empty = True
     return t_list_not_empty
 
-# Функция вывода меню
-def ShowAndChooseUserActions(t_menu_list): #
-    if len(t_menu_list) == 0:
-        t_exit_allow = True
-        print("Список заголовков меню пуст!")
-    print("Введите цифру для выбора варианта работы с заметками:")
-    for i in range(0, len(t_menu_list)):
-        print(f"'{i}' - {t_menu_list[i]};")
-    t_act = str(input("Выберите нужное число: > "))
-    return t_act
 
-# Функция форматированного отображения всех заметок
-def ShowAllNotes(t_lists): #
-    print("===== Перечень заметок: =====")
-    for i in range(0, len(t_lists)):
-        ShowNote(t_lists[i], t_lists.index(t_lists[i]))
-        # return t_act
-
-def EditNote(t_list):
+def edit_note(t_list):
     print("===== Редактирование заметки: =====")
+
 
 # -= ТЕКСТ ПРОГРАММЫ =-
 # Объявление списков
-a_title_list = []                                         # Вложенный список для заголовков
-a_status_list = ("в процессе","выполнено","отложено")     # Кортеж возможных статусов заметки
-# a_note = ["", "", "в процессе", "", "", a_title_list, 0]  # Список для хранения одной заметки, 7-й элемент - число заголовков
-a_note = {"1" : "", "2" : "", "3" : "", "4" : "", "5" : "", "6" : a_title_list, "7" : 0}
-a_notes = []                                              # Список всех заметок
-# a_list_empty = True # По умолчанию считаем, что список с заметками пуст
+a_title_list = []  # Вложенный список для заголовков
+a_status_list = ("в процессе", "выполнено", "отложено")  # Кортеж возможных статусов заметки
+a_note = {"1": "", "2": "", "3": "", "4": "", "5": "", "6": a_title_list}  # Словарь для хранения одной заметки
+a_notes = []  # Список всех заметок
 a_ses_exit_allow = "1"  # Инициализация маркера выхода из программы
-a_menu_list = ["Выход", "Работа со списком заметок", "Работа с конкретной заметкой"]
 
 # Основной цикл программы, демонстрирующий меню для работы
 print("Приветствую Вас! Данная программа позволит работать с заметками.")
-if CheckNotesInList():
-    a_notes = LoadNotes()  # Загружаем заметки из файла, чтобы не возиться со вводом
-    ShowAllNotes(a_notes)  # Демонстрируем чего у нас в справочнике
-while a_ses_exit_allow == "1" :
-    a_main_act = "1"
-    a_act = "1" # По умолчанию предполагается ввод новой заявки
-
-    a_main_act = ShowAndChooseUserActions(a_menu_list) # Выведем меню для выбора режима работы
-    if a_main_act == "0": # Быстрый выход
+if check_notes_in_list():
+    a_notes = load_notes()  # Загружаем заметки из файла, чтобы не возиться со вводом
+    show_all_notes(a_notes)  # Демонстрируем чего у нас в справочнике
+while a_ses_exit_allow == "1":
+    a_act = "1"  # По умолчанию предполагается ввод новой заявки
+    a_menu_list = ["Выход", "Работа со списком заметок", "Работа с конкретной заметкой"]
+    a_main_act = show_choose_user_actions(a_menu_list)  # Выведем меню для выбора режима работы
+    if a_main_act == "0":  # Быстрый выход
         exit()
-    elif a_main_act == "1": # Работа со списком заметок
+    elif a_main_act == "1":  # Работа со списком заметок
         print("===== Работа со списком заметок =====")
-        a_menu_list = ["Выход", "Режим добавления новой заметки", "Режим отображения всех заметок", "Режим удаления заметки"]
-        a_act = ShowAndChooseUserActions(a_menu_list)  # Выведем меню для выбора режима работы
+        a_menu_list = ["Выход", "Режим добавления новой заметки", "Режим отображения всех заметок",
+                       "Режим удаления заметки"]
+        a_act = show_choose_user_actions(a_menu_list)  # Выведем меню для выбора режима работы
         if a_act == "0":  # Быстрый выход
             exit()
-        elif a_act == "1": # Режим ввода новой заметки
-            NewNote(a_notes, a_note)
-        elif a_act == "2": # Режим отображения всех заметок
-            ShowAllNotes(a_notes)
-        elif a_act == "3":# Режим удаления заметки
+        elif a_act == "1":  # Режим ввода новой заметки
+            new_note(a_notes, a_note)
+        elif a_act == "2":  # Режим отображения всех заметок
+            show_all_notes(a_notes)
+        elif a_act == "3":  # Режим удаления заметки
             print("Извините, данный режим пока не доступен")  # Здесь пока заглушка
         else:
             print("Вы ввели некорректное число, режим не был активирован.")
             t_exit_allow = False
-    elif a_main_act == "2": # Работа с конкретной заметкой
+    elif a_main_act == "2":  # Работа с конкретной заметкой
         print("===== Работа с конкретной заметкой =====")
         print("Извините, данный режим пока не доступен")
         # a_menu_list = ["Выход", "Режим редактирования заметки","Режим работы со статусом заметки","Режим работы с датами заметки"]
-        # a_act = ShowAndChooseUserActions(a_menu_list)  # Выведем меню для выбора режима работы
+        # a_act = show_choose_user_actions(a_menu_list)  # Выведем меню для выбора режима работы
         # Здесь будет поиск заметки по ??? и работа с нею
         # if a_act == "0": # Быстрый выход
         #     exit()
         # elif a_act == "1": # Режим редактирования заметки
-        #     EditNote(a_note)
+        #     edit_note(a_note)
         # elif a_act == "2": # Режим работы со статусом заметки
         #     print("Извините, данный режим пока не доступен") # Здесь пока заглушка
-        #     # a_note = StatusChange(a_note)
+        #     # a_note = status_change(a_note)
         # elif a_act == "3":# Режим работы с датами заметки
         #     print("Извините, данный режим пока не доступен")  # Здесь пока заглушка
-        #     # a_note = DataChange(a_note)
+        #     # a_note = data_change(a_note)
         # else:
         #     print("Вы ввели некорректное число, режим не был активирован.")
         #     t_exit_allow = False
